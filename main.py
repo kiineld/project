85ddc2dfc9fea62c0e2271d55a7c5ea09481f348
kiineld <yaromyr.akimyv@gmail.com>
2024-02-21 18:04:57 +0300

    udalil imports

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 from kivy.core.window import Window
 from kivymd.app import MDApp
-from kivy.uix.textinput import TextInput
-from kivy.uix.button import Button
-from kivy.uix.label import Label
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.graphics import Color, Line
